26d364841215e5fe27b34f6bed39341b00ba2461
Vidya Sagar <mvsr222@gmail.com>
2020-08-22 07:48:19 +0530

    Pagination issue - reloading previous page

--- client/src/app/collection/collection.component.ts
+++ client/src/app/collection/collection.component.ts
@@ -56,13 +56,11 @@ export class CollectionComponent implements OnInit {
     this.API.deleteDocumentById(this.database, this.collection, id).subscribe(
       () => {
         this.message.info('Deleted!');
-        this.data.count -= 1;
-        if (!(this.data.count % 10))
+        if (!((this.data.count - 1) % 10) && (this.data.count - 1) < (this.pageIndex * 10))
         {
           if (this.pageIndex != 1)
             this.pageIndex -= 1;
-        }
-        this.query();
+        }        this.query();
       }
     );
   }
@@ -84,8 +82,6 @@ export class CollectionComponent implements OnInit {
           (response) => {
             this.closeEditor();
             this.message.success('A new document has been added');
-            this.data.count += 1;
-            this.pageIndex = Math.floor(this.data.count / 10) + 1;
             this.query();
           }
         );
