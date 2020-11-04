35ef15fb902e4a7368496429a93ff3d27fe3d8ee
patrix1337 <21961651+patrix1337@users.noreply.github.com>
2020-11-05 00:21:54 +0100

    disable failing healthcheck

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json());
 app.use('/databases', databasesRoute);
 
 // serve home page
-app.get('/', (req, res) => res.sendFile(__dirname + '../public/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 // connect to database
 dataAccessAdapter.InitDB(app);
